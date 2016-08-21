f59a29cca084ec2b9c7a0bfecb7858c8624f9e40
CongeeCafe <k.zhou@hotmail.ca>
2016-08-21 01:41:09 -0400

    Working sword modification and download

--- server/script.py
+++ server/script.py
@@ -1,4 +1,36 @@
-# Bring in Blender's Python API
 import bpy
+from bpy import context
+import os
+import sys
+import time
 
-print("This is a blender script")
+argv = sys.argv
+argv = argv[argv.index("--") + 1:]  # get all args after "--"
+
+ob = bpy.data.objects["Object"]
+mesh = ob.data
+
+#root_dir = os.path.dirname(os.path.realpath(__file__))
+#filepath = os.path.join(root_dir, 'generated', 'model' + '_' + str(int(time.time())) + '.obj')
+
+filepath = argv[0]
+
+# Extend sword
+for vert in mesh.vertices:
+	new_location = vert.co
+	if new_location[0] > 0:
+		new_location[0] = new_location[0] + 1 #X
+		#new_location[1] = new_location[1] + 1 #Y
+		#new_location[2] = new_location[2] + 1 #Z
+		vert.co = new_location
+
+# Export as OBJ
+with open(filepath, 'w') as f:
+	f.write("# OBJ file\n")
+	for v in mesh.vertices:
+		f.write("v %.4f %.4f %.4f\n" % v.co[:])
+	for p in mesh.polygons:
+		f.write("f")
+		for i in p.vertices:
+			f.write(" %d" % (i + 1))
+		f.write("\n")
--- server/server.py
+++ server/server.py
@@ -1,17 +1,20 @@
 import os
 from flask import Flask, request, send_from_directory, make_response
 from flask_cors import CORS, cross_origin
+import time
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/")
 def hello():
-	os.system('blender models/pepsi/Lata.blend')
-
 	root_dir = os.path.dirname(os.path.realpath(__file__))
-	print (os.path.join(root_dir, 'generated'))
-	response = make_response('http://10.71.134.218:5000/generated/text.txt')
+	filename = 'model' + '_' + str(int(time.time())) + '.obj'
+	filepath = os.path.join(root_dir, 'generated', filename)
+
+	os.system('blender -b models/sword/sword.blend --python script.py -- ' + filepath)
+
+	response = make_response('http://10.71.134.218:5000/generated/' + filename)
 	return response
 
 @app.route("/generated/<path:filename>")
